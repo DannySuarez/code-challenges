806fc03e6f935c7c10bbc60fabb72fb79ddcfecf
DannySuarez <suarezd10@gmail.com>
2019-12-10 10:05:09 -0800

    Add tests for Linked list class

--- linkedLists/LinkedList.test.js
+++ linkedLists/LinkedList.test.js
@@ -1,8 +1,14 @@
-const LinkedList = require('./LinkedList');
+const { LinkedList } = require('./LinkedList');
 
 describe('Linked List Class', () => {
+  const linkedList = new LinkedList();
   it('instantiates an empty list', () => {
-    expect(LinkedList.head).toBeNull;
+    expect(linkedList.head).toBeNull;
+  });
+
+  it('inserts into a linked list', () => {
+    linkedList.insert(0);
+    expect(linkedList.head).toEqual({ 'next': null, 'value': 0 });
   });
   
 });
