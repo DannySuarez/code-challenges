c78ee638b71123f91ce274362e6388b19201dc35
DannySuarez <suarezd10@gmail.com>
2019-12-10 09:43:09 -0800

    pass empty list test

---
+++ linkedLists/LinkedList.test.js
@@ -0,0 +1,8 @@
+const LinkedList = require('./LinkedList');
+
+describe('Linked List Class', () => {
+  it('instantiates an empty list', () => {
+    expect(LinkedList.head).toBeNull;
+  });
+  
+});
