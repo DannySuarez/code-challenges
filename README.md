a667fb347fe698dcbcd13ef2ab9bee25b2d739ee
DannySuarez <suarezd10@gmail.com>
2020-02-02 08:09:37 -0800

    add test to verify for null

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A collection of code challenges
 # Linked List
 Implement the data structure of a linked list class and methods<br />
 Tested methods
+
+# Binary Search
+Created a function that searches an array for a given value and returns the index if found.<br />
+Returns null if item is not in the array
--- binarySearch/binarySearch.test.js
+++ binarySearch/binarySearch.test.js
@@ -1,11 +1,17 @@
-const binary = require('./binary');
+const binarySearch = require('./binarySearch');
 
 describe('it will test binary searches', () => {
   it('will return the index where the value lives', () => {
     const myNumbers = [1, 3, 4, 5, 6, 7, 8, 9, 34]; 
 
-    const result = binary(myNumbers, 8);
+    const result = binarySearch(myNumbers, 8);
     expect(result).toEqual(6);
+  });
 
+  it('will return null if value is not in the Array', () => {
+    const myNumbers = [9, 8, 7, 6, 5];
+    const result = binarySearch(myNumbers, 1);
+
+    expect(result).toEqual(null);
   });
 });
